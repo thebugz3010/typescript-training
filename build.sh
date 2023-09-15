6b57bf6430a57dbe46d8e710261f111ee90307f8
Thomas <auzet.t@gmail.com>
2023-09-15 10:23:19 +0800

    [Build] Adjusted build script

--- build.sh
+++ build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-cp -R src/views build/
-cp -R src/public build/
+BUILD_FOLDER=build
+if [ ! -d $BUILD_FOLDER ]; then
+	mkdir $BUILD_FOLDER
+fi
+cp -R src/views $BUILD_FOLDER
+cp -R src/public $BUILD_FOLDER
 npx tsc
